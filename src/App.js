bdc55465901416dde3264919b11cf56b2033e729
rahee-k-it <108662234+rahee-k-it@users.noreply.github.com>
2022-07-25 11:37:09 +0900

    develop 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import { Reset } from 'styled-reset';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <React.Fragment>
+    <Reset />
+    <div>Hi, I'm an app!</div>
+  </React.Fragment>
+);
 
 export default App;
