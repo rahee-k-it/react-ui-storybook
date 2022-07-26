7659d70d7e2dc14e6457a0e6a74dbfb68c4dad39
Chi Hwan Kim <rlaclghks123@naver.com>
2022-07-26 15:21:09 +0900

    globalStyle적용

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,19 @@
+import GlobalStyles from '../src/GlobalStyles';
+import { addDecorator } from '@storybook/react';
+
+addDecorator((story) => (
+  <>
+    <GlobalStyles />
+    {story()}
+  </>
+));
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
