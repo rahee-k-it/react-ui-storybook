00d5142ffbaf6da36765765b155a51368e1d36e8
rahee-k-it <108662234+rahee-k-it@users.noreply.github.com>
2022-07-27 21:52:58 +0900

    보더 카드, 쉐도우 카드 예시 추가

--- src/components/Card/Card.stories.jsx
+++ src/components/Card/Card.stories.jsx
@@ -8,9 +8,21 @@ export default {
 const Template = (args) => <Card {...args} />;
 
 export const Default = Template.bind({});
-
 export const ColorCard = Template.bind({});
+export const BorderCard = Template.bind({});
+export const ShadowCard = Template.bind({});
+
 ColorCard.args = {
   headerColor: '#ff9f43',
   bodyColor: '#F8EFBA',
+};
+
+BorderCard.args = {
+  borderRadiusSize: 'xl',
+  border: true,
+};
+
+ShadowCard.args = {
+  shadow: true,
+  shadowDirection: 'down-right',
 };
