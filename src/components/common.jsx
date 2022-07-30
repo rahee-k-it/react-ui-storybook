718fe781f9b2bc15be301aabec8ef9ab28e472be
rahee-k-it <108662234+rahee-k-it@users.noreply.github.com>
2022-07-30 15:38:42 +0900

    feat: common.jsx에 backgroundColors 추가

--- src/components/common.jsx
+++ src/components/common.jsx
@@ -28,6 +28,38 @@ export const fontColors = [
   'text-blue-900',
 ];
 
+export const backgroundColors = [
+  'bg-inherit',
+  'bg-current',
+  'bg-transparent',
+  'bg-black',
+  'bg-white',
+
+  'bg-gray-100',
+  'bg-gray-300',
+  'bg-gray-500',
+  'bg-gray-700',
+  'bg-gray-900',
+
+  'bg-red-100',
+  'bg-red-300',
+  'bg-red-500',
+  'bg-red-700',
+  'bg-red-900',
+
+  'bg-green-100',
+  'bg-green-300',
+  'bg-green-500',
+  'bg-green-700',
+  'bg-green-900',
+
+  'bg-blue-100',
+  'bg-blue-300',
+  'bg-blue-500',
+  'bg-blue-700',
+  'bg-blue-900',
+];
+
 export const fontSizes = [
   'text-xs', // font-size: 0.75rem;  12px  line-height: 1rem;  16px
   'text-sm', // font-size: 0.875rem;  14px  line-height: 1.25rem;  20px
