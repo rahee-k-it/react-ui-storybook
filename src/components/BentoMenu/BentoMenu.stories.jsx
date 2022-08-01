1de05317a09c5bd52cd471e288922c1699c971cd
Chi Hwan Kim <rlaclghks123@naver.com>
2022-08-01 23:04:30 +0900

    argTypes 및 Default의 args입력

--- src/components/BentoMenu/BentoMenu.stories.jsx
+++ src/components/BentoMenu/BentoMenu.stories.jsx
@@ -1,12 +1,46 @@
+import { backgroundColors, fontColors } from '../common';
 import BentoMenu from './BentoMenu';
 
 export default {
   title: 'BentoMenu',
   component: BentoMenu,
+  argTypes: {
+    titleColor: {
+      control: 'select',
+      options: fontColors,
+    },
+    wrapperBgColor: {
+      control: 'select',
+      options: backgroundColors,
+    },
+    containerBgColor: {
+      control: 'select',
+      options: backgroundColors,
+    },
+    gridTemplateColumns: {
+      control: 'select',
+      options: ['grid-cols-1', 'grid-cols-2', 'grid-cols-3', 'grid-cols-4', 'grid-cols-5'],
+    },
+    gridTemplateRows: {
+      control: 'select',
+      options: ['grid-rows-1', 'grid-rows-2', 'grid-rows-3', 'grid-rows-4', 'grid-rows-5'],
+    },
+  },
 };
 
 const Template = (args) => <BentoMenu {...args}></BentoMenu>;
 
 export const Default = Template.bind({});
 
-Default.args = {};
+Default.args = {
+  clickTitle: 'Bentos',
+  title: 'Menu',
+  wrapperWidth: 'w-96',
+  wrapperBgColor: 'bg-gray-100',
+  wrapperPadding: 'p-5',
+  containerBgColor: 'bg-white',
+  gridTemplateColumns: 'grid-cols-3',
+  gridTemplateRows: 'grid-rows-3',
+  gridAutoRows: 'auto-rows-fr',
+  titleColor: 'text-red-blue',
+};
