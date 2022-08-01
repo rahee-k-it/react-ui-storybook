eb5db559c8a3c6e7f5c0af9e5073597d3c5d5841
Chi Hwan Kim <rlaclghks123@naver.com>
2022-08-01 22:53:11 +0900

    기본 설정

--- src/components/BentoMenu/BentoMenu.jsx
+++ src/components/BentoMenu/BentoMenu.jsx
@@ -1,5 +1,79 @@
-function BentoMenu() {
-  return <></>;
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const ClickBox = styled.span``;
+
+const Wrapper = styled.div``;
+
+const Container = styled.div``;
+
+const Menu = styled.div``;
+
+const Box = styled.div``;
+
+const Icon = styled.div``;
+const Title = styled.div``;
+
+function BentoMenu({ title, clickTitle }) {
+  const [bento, setBento] = useState(false);
+
+  const onClick = () => {
+    setBento((bento) => !bento);
+  };
+  return (
+    <>
+      <ClickBox onClick={onClick}>
+        <span>{clickTitle}</span>
+      </ClickBox>
+      {bento ? (
+        <>
+          <Wrapper>
+            <Menu>{title}</Menu>
+            <Container>
+              <Box>
+                <Icon>1</Icon>
+                <Title>one</Title>
+              </Box>
+              <Box>
+                <Icon>2</Icon>
+                <Title>two</Title>
+              </Box>
+              <Box>
+                <Icon>3</Icon>
+                <Title>T</Title>
+              </Box>
+              <Box>
+                <Icon>1</Icon>
+                <Title>one</Title>
+              </Box>
+              <Box>
+                <Icon>1</Icon>
+                <Title>one</Title>
+              </Box>
+              <Box>
+                <Icon>1</Icon>
+                <Title>one</Title>
+              </Box>
+              <Box>
+                <Icon>1</Icon>
+                <Title>one</Title>
+              </Box>
+              <Box>
+                <Icon>1</Icon>
+                <Title>one</Title>
+              </Box>
+              <Box>
+                <Icon>1</Icon>
+                <Title>one</Title>
+              </Box>
+            </Container>
+          </Wrapper>
+        </>
+      ) : (
+        ''
+      )}
+    </>
+  );
 }
 
 export default BentoMenu;
