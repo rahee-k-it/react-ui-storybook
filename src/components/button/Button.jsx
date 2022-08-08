206afd5edf1915843524c1d0bd66581252426837
chiman <rlaclghks123@naver.com>
2022-08-08 21:08:18 +0900

    Button컴포넌트  icon부분 삭제

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,14 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-const svgMargins = {
-  none: '',
-  left: '0px 0px 0px 5px',
-  right: '0px 5px 0px 0px',
-  up: '5px 0px 0px 0px',
-  down: '0px 0px 5px 0px',
-};
 
 const hoverColors = {
   none: '',
@@ -23,9 +14,6 @@ const ButtonStyle = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  svg {
-    ${({ svgMargin }) => (svgMargin ? `margin:${svgMargins[svgMargin]}` : 'none')};
-  }
   :hover {
     ${({ hoverColor }) => (hoverColor ? `color:${hoverColors[hoverColor]}` : 'none')};
   }
@@ -39,8 +27,6 @@ function Button(props) {
     children = 'Button',
     className = '',
     hoverColor = '',
-    icon = '',
-    svgMargin = '0px 5px 0px 0px',
     fontSize = '',
     fontWeight = '',
     flexDirection = 'flex-row-reverse',
@@ -52,18 +38,10 @@ function Button(props) {
   return (
     <ButtonStyle
       className={` ${bgColor} ${borderRadius} ${className} ${color} ${fontWeight} ${flexDirection} ${fontSize} ${padding} ${textDecoration} `}
-      svgMargin={svgMargin}
       hoverColor={hoverColor}
       {...rest}
     >
-      {icon ? (
-        <>
-          <span>{children}</span>
-          <FontAwesomeIcon icon={icon} />
-        </>
-      ) : (
-        <span>{children}</span>
-      )}
+      {children}
     </ButtonStyle>
   );
 }
