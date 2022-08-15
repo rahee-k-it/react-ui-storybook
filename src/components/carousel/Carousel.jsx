189347aaacde62091283823d75b8b02b7064abdc
치맨 <rlaclghks123@naver.com>
2022-08-15 14:15:59 +0900

    left,right,autoPlay 구현

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const CarouselContainer = styled.div`
   display: flex;
@@ -10,12 +12,70 @@ const CarouselContainer = styled.div`
   ${({ width }) => (width ? `width: ${width}px` : '200px')};
 `;
 
-function Carousel({ className, carouselContainerWidth = 200, ...others }) {
+const LeftBtn = styled.button`
+  cursor: pointer;
+  opacity: ${(props) => props.opacity};
+  svg {
+    position: absolute;
+    z-index: 2;
+    top: 50%;
+    left: 10px;
+  }
+`;
+
+const RightBtn = styled.button`
+  cursor: pointer;
+  opacity: ${(props) => props.opacity};
+  svg {
+    position: absolute;
+    z-index: 2;
+    top: 50%;
+    right: 10px;
+  }
+`;
+
+const AutoBox = styled.div`
+  opacity: 0;
+`;
+
+function Carousel({
+  className,
+  carouselContainerWidth = 200,
+  childrens,
+  itemWidth = 200,
+  autoPlay,
+  ...others
+}) {
   let widthRatio = carouselContainerWidth;
+
+  const [transForm, setTransForm] = useState(0);
+
+  const length = childrens.length;
+  const endLength = -(length * itemWidth) + carouselContainerWidth;
+
+  const onClickLeft = () => {
+    setTransForm((trans) => (trans >= 0 ? endLength : (trans += 200)));
+  };
+
+  const onClickRight = () => {
+    setTransForm((trans) => (trans <= endLength ? (trans = 0) : (trans -= 200)));
+  };
+
+  const onAutoPlay = () => {
+    setTransForm((trans) => (trans < endLength ? (trans = 0) : (trans -= 200)));
+  };
 
   return (
     <>
       <CarouselContainer width={widthRatio} className={className} {...others}></CarouselContainer>
+
+      <LeftBtn onClick={onClickLeft} opacity={autoPlay ? 0 : 1}>
+        <FontAwesomeIcon icon={faArrowLeft} />
+      </LeftBtn>
+      <RightBtn onClick={onClickRight} opacity={autoPlay ? 0 : 1}>
+        <FontAwesomeIcon icon={faArrowRight} />
+      </RightBtn>
+      <AutoBox>{autoPlay ? setTimeout(onAutoPlay, 2000) : ''}</AutoBox>
     </>
   );
 }
