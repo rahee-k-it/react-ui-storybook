3b0dd5a3107b04879312180a3e828d27f1335480
Heo Youngseo <dudtj12kr@gmail.com>
2022-10-04 16:05:14 +0900

    preview.js 파일에 올바르지 않은 문자열 들어가는 이슈 해결 / 기존 라이브러리 외 새로운 라이브러리 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,7 @@
 import GlobalStyles from '../src/GlobalStyles';
 import { addDecorator } from '@storybook/react';
 import '../src/assets/styles/tailwind.css';
-<<<<<<< HEAD
 import '../src/assets/styles/styles.scss';
-=======
->>>>>>> 84db2ba6d120c4ea232424607ff92b3cd7497418
 
 addDecorator((story) => (
   <>
