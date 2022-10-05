5e93f82dcd8ddb8f4d7ae1eefc3e2eb295b449e5
rahee-k-it <rahee.k.it@gmail.com>
2022-10-05 20:29:24 +0900

    feat: modal UI 제작

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  background-color: #000000a1;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Modal = ({ children, offModal, isModalOpened }) => {
+  return <>{isModalOpened ? <Container onClick={offModal}>{children}</Container> : null}</>;
+};
+
+Modal.defaultProps = {
+  offModal: '',
+  isModalOpened: '',
+};
+
+export default Modal;
