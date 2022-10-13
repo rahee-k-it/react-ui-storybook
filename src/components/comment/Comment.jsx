8513a23f4f9ed32084f993a9a4c8003798f1c94a
daep93 <eoy1313@gmail.com>
2022-10-13 17:51:51 +0900

    refactor:  변수명 변경 및 코드 정리

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -1,28 +1,10 @@
 import styled from 'styled-components';
 
-const CommentContainer = styled.div`
-  &:first-child {
-    border-top: 1px solid rgba(0, 0, 0, 0.2);
-  }
-  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
-`;
-
 const ProfileImageBox = styled.img`
   background-image: url(${(props) => props.profileImg ?? './images/basicCharacter.png'});
   background-position: center;
   background-size: cover;
 `;
-
-const CommentContents = styled.div``;
-
-const ContentsHeaderContainer = styled.div``;
-
-const ContentsMainBox = styled.div``;
-
-const ContentsTitle = styled.div`
-  font-weight: 900;
-`;
-
 const TimeStamp = styled.div`
   display: flex;
   align-items: center;
@@ -30,71 +12,40 @@ const TimeStamp = styled.div`
   font-size: 12px;
 `;
 
-function timeForToday(createdAt) {
-  const today = new Date();
-  const getElapsedTimeFromCommentCreated = new Date(createdAt);
-
-  const ElapsedMin = Math.floor(
-    (today.getTime() - getElapsedTimeFromCommentCreated.getTime()) / 1000 / 60,
-  );
-  if (ElapsedMin < 1) return '방금 전';
-  if (ElapsedMin < 60) {
-    return `${ElapsedMin}분 전`;
-  }
-
-  const ElapsedHour = Math.floor(ElapsedMin / 60);
-  if (ElapsedHour < 24) {
-    return `${ElapsedHour}시간 전`;
-  }
-
-  const ElapsedHourDay = Math.floor(ElapsedMin / 60 / 24);
-  if (ElapsedHourDay < 30) {
-    return `${ElapsedHourDay}일 전`;
-  }
-
-  const ElapsedHourMonth = Math.floor(ElapsedMin / 60 / 24 / 30);
-  if (ElapsedHourMonth < 12) {
-    return `${ElapsedHourMonth}달 전`;
-  }
-  if (Math.floor(ElapsedHourDay / 365) === 0) {
-    return '12달전';
-  } else {
-    return `${Math.floor(ElapsedHourDay / 365)}년 전`;
-  }
+function getElapsedTimeFromCommentCreated(createdAt) {
+  const elapsedTimeInSecond = (new Date().getTime() - new Date(createdAt).getTime()) / 1000;
+  const elapsedTimeInMinute = Math.floor(elapsedTimeInSecond / 60);
+  if (elapsedTimeInMinute < 1) return '방금 전';
+  if (elapsedTimeInMinute < 60) return `${elapsedTimeInMinute}분 전`;
+  const elapsedTimeInHour = Math.floor(elapsedTimeInSecond / (60 * 60));
+  if (elapsedTimeInHour < 24) return `${elapsedTimeInHour}시간 전`;
+  const elapsedTimeInDay = Math.floor(elapsedTimeInSecond / (60 * 60 * 24));
+  if (elapsedTimeInDay < 30) return `${elapsedTimeInDay}일 전`;
+  const elapsedTimeInInMonth = Math.floor(elapsedTimeInSecond / (60 * 60 * 24 * 30));
+  if (elapsedTimeInInMonth < 12) return `${elapsedTimeInInMonth}달 전`;
+  return `${Math.floor(elapsedTimeInDay / 365)}년 전`;
 }
 
 function Comment({
-  commentContainerClassName,
-
-  profileImg,
-  profileClassName,
-
-  commentContentsClassName,
-  commentContentsTitle,
-
-  createdAt,
-  contentsMainClassName,
   children,
+  createdAt,
+  profileImg,
+  title,
+  containerClassName = '',
+  profileClassName = '',
+  contentsClassName = '',
 }) {
   return (
-    <CommentContainer className={`flex p-5 ${commentContainerClassName}`}>
-      <ProfileImageBox
-        profileImg={profileImg}
-        className={`${profileClassName ?? ''}`}
-      ></ProfileImageBox>
-
-      <CommentContents className={`flex flex-col p-1 ${commentContentsClassName ?? ''}`}>
-        <ContentsHeaderContainer className={`flex flex-start`}>
-          <ContentsTitle className={commentContentsTitle ? 'mr-3' : ''}>
-            {commentContentsTitle}
-          </ContentsTitle>
-
-          {createdAt ? <TimeStamp>{timeForToday(createdAt)}</TimeStamp> : null}
-        </ContentsHeaderContainer>
-
-        <ContentsMainBox className={`${contentsMainClassName ?? ''}`}>{children}</ContentsMainBox>
-      </CommentContents>
-    </CommentContainer>
+    <div className={`flex p-5 ${containerClassName}`}>
+      <ProfileImageBox profileImg={profileImg} className={profileClassName} />
+      <div className={`flex flex-col p-1`}>
+        <div className={`flex flex-start`}>
+          <p className={`font-bold ${title ? 'mr-3' : ''}`}>{title}</p>
+          {createdAt ? <TimeStamp>{getElapsedTimeFromCommentCreated(createdAt)}</TimeStamp> : null}
+        </div>
+        <div className={contentsClassName}>{children}</div>
+      </div>
+    </div>
   );
 }
 
