8c4395eadb516b8ec77f9351d013cf0e5bd2a312
rahee-k-it <rahee.k.it@gmail.com>
2022-10-16 13:06:32 +0900

    feat: loader UI 구현

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,134 +1,58 @@
 import styled, { keyframes } from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faSpinner,
-  faAppleWhole,
-  faCloudArrowDown,
-  faHourglassStart,
-  faHourglassHalf,
-  faHourglassEnd,
-} from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
 
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  position: fixed;
-  /* background-color: #cd92bca0; */
-  top: 0;
-  left: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Text = styled.div`
-  font-size: 16px;
-  margin-top: 10px;
-`;
-
-const rotation = keyframes`
-    from{
-        transform: rotate(0deg);
-    }
-
-    to{
-        transform: rotate(360deg);
-    }
-
+const spinEffect = keyframes`
+  from {transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
 `;
 
 const Spinner = styled.div`
-  font-size: 30px;
   display: flex;
   justify-content: center;
   align-items: center;
-  animation: ${rotation} 1.5s linear infinite;
-  /* margin-bottom: 10px; */
+  animation: ${spinEffect} 1.5s linear infinite;
 `;
 
-const colorChange = keyframes` 
-0%{
-    color: #a4a8a4;
-}
-50%{ 
-    color: #666b66;
-}
-100%{
-    color: #000000;
-}
+const winkEffect = keyframes` 
+  0% { opacity: 0.2 }
+  100% { opacity: 1; } 
 `;
 
-const Color = styled.div`
-  font-size: 30px;
+const Winker = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  animation: ${colorChange} 2s linear infinite;
-  /* margin-bottom: 10px; */
+  animation: ${winkEffect} 2s ease-in-out infinite alternate;
 `;
 
-const motion = keyframes`
-    from{
-        margin-top: 0px;
-    }
-
-    to{
-      margin-top: 50px;
-    }
-
+const floatEffect = keyframes`
+  from { transform: translateY(0) }
+  to { transform: translateY(-10px) }
 `;
 
-const UpDown = styled.div`
-  font-size: 30px;
+const Floater = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  animation: ${motion} 2s linear infinite alternate;
+  animation: ${floatEffect} 1.5s linear infinite alternate;
 `;
 
-const Loader = () => {
+const Loader = ({ effect, icon, children }) => {
+  const [effectComponent, setEffectComponent] = useState('');
+  useEffect(() => {
+    if (effect === 'spin') setEffectComponent(<Spinner>{icon}</Spinner>);
+    else if (effect === 'wink') setEffectComponent(<Winker>{icon}</Winker>);
+    else if (effect === 'float') setEffectComponent(<Floater>{icon}</Floater>);
+    else setEffectComponent(<div>{icon}</div>);
+  }, [effect, icon]);
+
   return (
-    <>
-      <Container>
-        <Spinner>
-          <FontAwesomeIcon icon={faSpinner} />
-        </Spinner>
-        <Text>loading</Text>
-      </Container>
-    </>
+    <div className="w-screen h-screen fixed top-0 left-0 flex flex-col justify-center items-center">
+      {effectComponent}
+      <p className="mt-[10px]">{children}</p>
+    </div>
   );
 };
 
-const LoaderApple = () => {
-  return (
-    <>
-      <Container>
-        <Color>
-          <FontAwesomeIcon icon={faAppleWhole} />
-          <Text>loading</Text>
-        </Color>
-      </Container>
-    </>
-  );
-};
-
-const LoaderUpDown = () => {
-  return (
-    <>
-      <Container>
-        <UpDown>
-          {/* <Spinner> */}
-          <FontAwesomeIcon icon={faCloudArrowDown} />
-
-          {/* <FontAwesomeIcon icon={faFan} /> */}
-          {/* </Spinner> */}
-        </UpDown>
-        <Text>loading</Text>
-      </Container>
-    </>
-  );
-};
-
-export { Loader, LoaderApple, LoaderUpDown };
+export default Loader;
--- src/components/loader/Loader.stories.jsx
+++ src/components/loader/Loader.stories.jsx
@@ -1,31 +1,69 @@
-import { Loader, LoaderApple, LoaderUpDown } from './Loader';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner, faAppleWhole, faCloudArrowDown } from '@fortawesome/free-solid-svg-icons';
+import Loader from './Loader';
+import { fontColors, fontSizes } from '../common';
+import styled from 'styled-components';
+const icons = {
+  spinner: <FontAwesomeIcon icon={faSpinner} />,
+  apple: <FontAwesomeIcon icon={faAppleWhole} />,
+  cloud: <FontAwesomeIcon icon={faCloudArrowDown} />,
+  undefined: '',
+};
 
 export default {
   title: 'Loader',
   component: Loader,
-  argTypes: {},
+  argTypes: {
+    effect: { control: 'select', options: ['spin', 'wink', 'float', 'undefined'] },
+    icon: {
+      options: Object.keys(icons),
+      mapping: icons,
+      control: { type: 'select' },
+    },
+    message: { control: 'text' },
+    iconSize: {
+      control: 'select',
+      options: ['sm', 'md', 'lg'],
+      mapping: { sm: '16px', md: '24px', lg: '32px' },
+    },
+    iconColor: { control: 'select', options: fontColors },
+    messageSize: { control: 'select', options: fontSizes },
+    messageColor: { control: 'select', options: fontColors },
+  },
 };
 
-export const IconSpin = () => {
+const Span = styled.span`
+  font-size: ${({ iconSize }) => iconSize};
+`;
+export const Default = ({
+  message,
+  effect,
+  icon,
+  iconSize,
+  iconColor,
+  messageSize,
+  messageColor,
+}) => {
   return (
-    <>
-      <Loader></Loader>
-    </>
+    <Loader
+      effect={effect}
+      icon={
+        <Span iconSize={iconSize} className={`${iconColor}`}>
+          {icon}
+        </Span>
+      }
+    >
+      <span className={`${messageSize} ${messageColor}`}>{message}</span>
+    </Loader>
   );
 };
 
-export const IconColorChange = () => {
-  return (
-    <>
-      <LoaderApple></LoaderApple>
-    </>
-  );
-};
-
-export const IconUpAndDown = () => {
-  return (
-    <>
-      <LoaderUpDown></LoaderUpDown>
-    </>
-  );
+Default.args = {
+  effect: 'spin',
+  icon: 'spinner',
+  message: 'loading...',
+  iconSize: 'sm',
+  iconColor: 'text-black',
+  messageSize: 'text-base',
+  messageColor: 'text-black',
 };
