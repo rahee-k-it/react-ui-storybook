22a8539f529837d4dc398731144c9d72b92ff6a7
rahee-k-it <rahee.k.it@gmail.com>
2022-10-24 19:00:56 +0900

    feat: form 컴포넌트 구현

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,117 +1,41 @@
+import { useCallback } from 'react';
 import styled from 'styled-components';
-// import Default from './Button,stories';
-import Button from '../button/Button';
 
-const Bg = styled.div`
-  width: 100vw;
-  height: 100vh;
-  /* background-color: #a4eada9f; */
-  position: fixed;
-  top: 0;
-  left: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const FormContainer = styled.div`
-  padding: 30px 0px;
-  width: 300px;
-  min-height: 400px;
-  background-color: white;
+const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  border-radius: 50px;
-  box-shadow: 0px 0px 5px 5px #e0e3da;
 `;
-
 const Title = styled.div`
-  font-weight: 900;
-  font-size: 35px;
   width: 250px;
   height: 60px;
-  /* background-color: #91dbdf; */
   display: flex;
   justify-content: center;
   align-items: center;
-  /* margin: 50px 0px 10px 0px; */
 `;
 
-const FormBoxs = styled.div`
-  width: 250px;
-  min-height: 10px;
-  /* background-color: #aadf91; */
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 20px 0px;
-`;
-
-const FormBox = styled.div`
-  width: 240px;
-  height: 60px;
-  /* background-color: #1cea7f; */
-  margin: 5px;
-`;
-
-const InputName = styled.div`
-  font-size: 16px;
-  height: 15px;
-  background-color: white;
-  z-index: 1;
-  position: absolute;
-  margin-left: 30px;
-`;
-
-const Input = styled.input`
-  font-size: 20px;
-  width: 240px;
-  height: 50px;
-  background-color: white;
-  border: 1px solid gray;
-  border-radius: 50px;
-  position: relative;
-  margin-top: 5px;
-  padding-top: 10px;
-  padding-left: 20px;
-`;
-
-const Form = () => {
+const Form = ({ title, onSubmit, titleColor, titleFontSize, titleFontWeight, children }) => {
+  const handleSubmit = useCallback((e) => {
+    // 새로고침 방지
+    e.preventDefault();
+    onSubmit && onSubmit(e);
+  }, []);
   return (
-    <Bg>
-      <FormContainer>
-        <Title>Create Account</Title>
-        <FormBoxs>
-          <FormBox>
-            <InputName>Full Name</InputName>
-            <Input></Input>
-          </FormBox>
-          <FormBox>
-            <InputName>Email Address</InputName>
-            <Input></Input>
-          </FormBox>
-          <FormBox>
-            <InputName>Phone Number</InputName>
-            <Input></Input>
-          </FormBox>
-        </FormBoxs>
-
-        <Button
-          bgColor="bg-blue-700"
-          fontSize="text-xl"
-          color="text-white"
-          padding="p-3"
-          borderRadius="rounded-full"
-        >
-          Create Account
-        </Button>
-      </FormContainer>
-    </Bg>
+    <FormContainer onSubmit={handleSubmit}>
+      {title ? (
+        <Title className={`${titleColor} ${titleFontSize} ${titleFontWeight}`}>{title}</Title>
+      ) : null}
+      {children}
+    </FormContainer>
   );
 };
 
-Form.defaultProps = {};
+Form.defaultProps = {
+  onSubmit: (e) => {},
+  titleColor: 'text-black',
+  titleFontSize: 'text-2xl',
+  titleFontWeight: 'font-black',
+};
 
 export default Form;
--- src/components/form/Form.stories.jsx
+++ src/components/form/Form.stories.jsx
@@ -1,23 +1,107 @@
 import Form from './Form';
-// import { fontColors, backgroundColors, fontSizes, fontWeights, borderRadiuses } from '../common';
 import styled from 'styled-components';
-// import { useCallback, useState } from 'react';
+import Button from '../button/Button';
+import { fontColors, fontSizes, fontWeights } from '../common';
 
 export default {
   title: 'Form',
   component: Form,
-  argTypes: {},
+  argTypes: {
+    title: { control: 'text' },
+    onSubmit: { table: { disable: true } },
+    inputs: { control: 'object' },
+
+    titleColor: { control: 'select', options: fontColors },
+    titleFontSize: { control: 'select', options: fontSizes },
+    titleFontWeight: { control: 'select', options: fontWeights },
+  },
 };
 
-const Template = (args) => <Form {...args} />;
+const FormBoxs = styled.div`
+  width: 250px;
+  min-height: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px 0px;
+`;
 
-export const Default = Template.bind({});
+const FormBox = styled.div`
+  width: 240px;
+  height: 60px;
+  margin: 5px;
+`;
 
-// const Button = styled.button`
-//   border-radius: 5px;
-//   height: 30px;
-//   color: blue;
-//   :hover {
-//     background-color: #b1d8f9;
-//   }
-// `;
+const InputName = styled.div`
+  font-size: 16px;
+  height: 15px;
+  background-color: white;
+  z-index: 1;
+  position: absolute;
+  margin-left: 30px;
+`;
+
+const Input = styled.input`
+  font-size: 20px;
+  width: 240px;
+  height: 50px;
+  background-color: white;
+  border: 1px solid gray;
+  border-radius: 50px;
+  position: relative;
+  margin-top: 5px;
+  padding-top: 10px;
+  padding-left: 20px;
+`;
+
+const FormContainer = styled.div`
+  width: 300px;
+  min-height: 400px;
+  padding: 30px 0px;
+  background-color: white;
+  border-radius: 50px;
+  box-shadow: 0px 0px 5px 5px #e0e3da;
+`;
+
+export const Default = ({ title, inputs, titleColor, titleFontSize, titleFontWeight }) => {
+  return (
+    <FormContainer>
+      <Form
+        title={title}
+        onSubmit={() => {
+          console.log('hello!');
+        }}
+        titleColor={titleColor}
+        titleFontSize={titleFontSize}
+        titleFontWeight={titleFontWeight}
+      >
+        <FormBoxs>
+          {inputs.map((input) => (
+            <FormBox>
+              <InputName>{input.name}</InputName>
+              <Input type={input.type} />
+            </FormBox>
+          ))}
+        </FormBoxs>
+        <Button
+          bgColor="bg-blue-700"
+          fontSize="text-xl"
+          color="text-white"
+          padding="p-3"
+          borderRadius="rounded-full"
+          type="submit"
+        >
+          Create Account
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
+Default.args = {
+  title: 'Signup',
+  inputs: [
+    { name: 'Full Name', type: 'text' },
+    { name: 'Email', type: 'email' },
+    { name: 'Phone Number', type: 'tel' },
+  ],
+};
