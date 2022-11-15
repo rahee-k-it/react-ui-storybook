c7056f0c4ea1d56527d0200e0938aff1ae359f4e
rahee-k-it <rahee.k.it@gmail.com>
2022-11-15 19:38:57 +0900

    refactor: array.map을 통한 menu 컴포넌트 렌더링

--- src/components/menu/Menu.stories.jsx
+++ src/components/menu/Menu.stories.jsx
@@ -12,16 +12,17 @@ export default {
   },
 };
 
+const menuTypes = ['hamberger', 'kebab', 'meatballs', 'doner'];
+
 export const Default = (args) => (
   <div>
     <h1 className="font-bold text-lg mb-1">기본 예시</h1>
     <Menu {...args} className="mb-2" />
     <h1 className="font-bold text-lg mb-1">선택할 수 있는 메뉴</h1>
     <div className="flex gap-2">
-      <Menu type="hamberger" />
-      <Menu type="kebab" />
-      <Menu type="meatballs" />
-      <Menu type="doner" />
+      {menuTypes.map((type, index) => (
+        <Menu type={type} key={index} />
+      ))}
     </div>
   </div>
 );
